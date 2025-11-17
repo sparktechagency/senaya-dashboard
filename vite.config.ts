3cb9a7540f15cd39a440055c1022f2b41f1b0ab7
asifaowadud <asifaowadud@gmail.com>
2025-11-17 15:00:01 +0600

    mend

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   plugins: [tailwindcss()],
   server: {
-    host: true, // <-- allows access from your AWS IP
-    port: 5173, // optional, default is 5173
+    host: "0.0.0.0", // Listen on all interfaces
+    port: 5173,
   },
 });
