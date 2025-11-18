2c2722cf67b729d306af33b8c39d20e65609f06c
IsmailHossen87 <ismailhosen8757@gmail.com>
2025-11-18 10:12:57 +0600

    moving to offical repo

--- src/Page/CARBRAND/CreateCountry.tsx
+++ src/Page/CARBRAND/CreateCountry.tsx
@@ -74,7 +74,7 @@ export const CreateCountry = () => {
           disabled={creating}
           className="bg-blue-500 text-white px-6 py-3 rounded-md hover:bg-blue-600 transition w-full sm:w-auto"
         >
-          {creating ? "Creating....." : "Create"}
+          {creating ? "Creating..." : "Create"}
         </button>
       </form>
 
